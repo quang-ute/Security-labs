4da8baddef7c40e1529cb916ca968e0689e22706
quang-ute <57078914+quang-ute@users.noreply.github.com>
2021-09-02 21:18:18 +0700

    Update flag.c

--- Software/flag.c
+++ Software/flag.c
@@ -2,14 +2,14 @@
 #include <string.h>
 int main(int argc, char* argv[])
 {
-	int	 flag=0;
+    int	 flag=0;
     char buffer[16];
 
     strcpy(buffer,argv[1]);
 
     if (flag!=0)
-		printf("Modified\n");
+	printf("Modified\n");
     else
-		printf("Try again\n");
+	printf("Try again\n");
     return 0;
 } 
