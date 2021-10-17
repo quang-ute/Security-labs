674166b6592324354688014f7840c93b883ef84c
quang-ute <57078914+quang-ute@users.noreply.github.com>
2021-10-17 19:53:12 +0700

    Add files via upload

---
+++ Network/sniff-n-spoof/python-source/sniff3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from scapy.all import *
+def process_packet(pkt):
+	if pkt.haslayer(IP):
+		ip = pkt[IP]
+		print("IP: {} --> {}".format(ip.src, ip.dst))
+	if pkt.haslayer(TCP):
+		tcp = pkt[TCP]
+		print("	TCP port: {} --> {}".format(tcp.sport, tcp.dport))
+	elif pkt.haslayer(UDP):
+		udp = pkt[UDP]
+		print(" UDP port: {} --> {}".format(udp.sport, udp.dport))
+	elif pkt.haslayer(ICMP):
+		icmp = pkt[ICMP]
+		print(" ICMP type: {}".format(icmp.type))
+	else:
+		print("	Other protocol")
+
+sniff(iface='eth0', filter='ip', prn=process_packet)
