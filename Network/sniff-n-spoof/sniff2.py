6c4f276e8d898cac8c390ede622dfed8f93ff103
quang-ute <57078914+quang-ute@users.noreply.github.com>
2021-10-17 19:53:48 +0700

    Delete sniff2.py

--- Network/sniff-n-spoof/sniff2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-def process_packet(pkt):
-    pkt.show()
-    print("----------------")
-f = 'udp and dst portrange 50-55 or icmp'
-pkt=sniff(iface='eth0', filter=f, prn=process_packet)
-pkt.summary()
