9dbf4780c2f21e5ba3a58a005c6498bfe19cddf4
quang-ute <57078914+quang-ute@users.noreply.github.com>
2023-06-27 14:51:39 +0700

    Delete Web-security/Basic directory

--- Web-security/Basic/image_apache_server/bank32/getdata.php
+++
@@ -1,8 +0,0 @@
-<?php
- #header("Access-Control-Allow-Origin: http://www.bank99.com");
-
- echo "Time from Bank32: ".date("h:i:sa")
-?>
-
-
-
--- Web-security/Basic/image_apache_server/bank99/getdata.php
+++
@@ -1,8 +0,0 @@
-<?php
- #header("Access-Control-Allow-Origin: http://www.bank33.com");
-
- echo "Time from Bank99: ".date("h:i:sa")
-?>
-
-
-
